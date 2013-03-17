4bcf082befec387c7de1378ed28125508fa95948
DoubleMalt <christoph@web.crofting.com>
2013-03-17 16:43:04 +0100

    add finder for region

--- lib/digitalocean/region.rb
+++ lib/digitalocean/region.rb
@@ -7,5 +7,8 @@ module Digitalocean
       response = Digitalocean.request.get "regions"
       RecursiveOpenStruct.new(response.body, :recurse_over_arrays => true)
     end
+    def self.find(id)
+      self.all.regions.detect{|size| region.id == id}
+    end
   end
 end
