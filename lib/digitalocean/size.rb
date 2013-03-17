f303b88d956417e97386a1303c056dff867cca0e
DoubleMalt <christoph@web.crofting.com>
2013-03-17 16:33:57 +0100

    add finder for sizes

--- lib/digitalocean/size.rb
+++ lib/digitalocean/size.rb
@@ -7,5 +7,8 @@ module Digitalocean
       response = Digitalocean.request.get "sizes"
       RecursiveOpenStruct.new(response.body, :recurse_over_arrays => true)
     end
+    def self.find(id)
+      self.all.sizes.detect{|size| size.id == id}
+    end
   end
 end
