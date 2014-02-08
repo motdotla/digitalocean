8bacd6285ceb4d8c590c712a824e3f2599cdb102
Tomek Maszkowski <tomazy@go2.pl>
2014-02-08 15:13:24 -0300

    Added /events api

---
+++ spec/digitalocean/event_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Digitalocean::Event do
+  let(:subject)   { Digitalocean::Event }
+
+  describe "._find" do
+    let(:event_id) { "1234" }
+
+    before do
+      @url = subject._find(event_id)
+    end
+
+    it do
+      @url.should eq "https://api.digitalocean.com/events/#{event_id}/?client_id=client_id_required&api_key=api_key_required"
+    end
+  end
+end
