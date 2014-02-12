75d3ed5e6ec998f184cba2fffdec221ad6768c2e
scottmotte <scott@scottmotte.com>
2014-02-11 22:27:47 -0800

    Version bump - 1.0.2

--- spec/digitalocean_spec.rb
+++ spec/digitalocean_spec.rb
@@ -12,7 +12,7 @@ describe Digitalocean do
     it { subject.api_endpoint.should eq "https://api.digitalocean.com" }
     it { subject.client_id.should eq "client_id_required" }
     it { subject.api_key.should eq "api_key_required" }
-    it { subject::VERSION.should eq "1.0.1" }
+    it { subject::VERSION.should eq "1.0.2" }
   end
 
   describe "setting values" do
