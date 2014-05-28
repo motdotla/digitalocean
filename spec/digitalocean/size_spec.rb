8ed009f8f05947343263d76256b166a177ff5cca
Juan C. Müller <jcmuller@gmail.com>
2014-05-28 11:10:14 -0400

    Refactor specs a bit.
    - Use subject keyword
    - Use let instead of instance variables
    - Use `its` to check for subject.`attr`

--- spec/digitalocean/size_spec.rb
+++ spec/digitalocean/size_spec.rb
@@ -1,28 +1,17 @@
 require 'spec_helper'
 
 describe Digitalocean::Size do
-  let(:subject)   { Digitalocean::Size }
+  subject(:size) { described_class }
 
   describe "._all" do
-    before do
-      @url = subject._all
-    end
-
-    it do
-      @url.should eq "https://api.digitalocean.com/sizes/?client_id=client_id_required&api_key=api_key_required" 
-    end
+    let(:url) { size._all }
+    it { url.should eq "https://api.digitalocean.com/sizes/?client_id=client_id_required&api_key=api_key_required"  }
   end
 
   describe "._find" do
     let(:size_id) { "1234" }
+    let(:url) { size._find(size_id) }
 
-    before do
-      @url = subject._find(size_id)
-    end
-
-    it do
-      @url.should eq "https://api.digitalocean.com/sizes/#{size_id}?client_id=client_id_required&api_key=api_key_required"
-    end
+    it { url.should eq "https://api.digitalocean.com/sizes/#{size_id}?client_id=client_id_required&api_key=api_key_required" }
   end
-
 end
--- spec/digitalocean_spec.rb
+++ spec/digitalocean_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe Digitalocean do
-  subject { Digitalocean }
+  subject(:digitalocean) { described_class }
 
   describe "defaults" do
     before do
-      subject.client_id       = nil
-      subject.api_key         = nil
+      digitalocean.client_id = nil
+      digitalocean.api_key   = nil
     end
 
-    it { subject.api_endpoint.should eq "https://api.digitalocean.com" }
-    it { subject.client_id.should eq "client_id_required" }
-    it { subject.api_key.should eq "api_key_required" }
-    it { subject::VERSION.should eq "1.0.6" }
+    its(:api_endpoint) { should eq "https://api.digitalocean.com" }
+    its(:client_id) { should eq "client_id_required" }
+    its(:api_key) { should eq "api_key_required" }
+    it { digitalocean::VERSION.should eq "1.0.6" }
   end
 
   describe "setting values" do
@@ -20,11 +20,11 @@ describe Digitalocean do
     let(:api_key)     { "adf3434938492fjkdfj" }
 
     before do
-      subject.client_id   = client_id
-      subject.api_key     = api_key
+      digitalocean.client_id = client_id
+      digitalocean.api_key   = api_key
     end
 
-    it { subject.client_id.should eq client_id }
-    it { subject.api_key.should eq api_key }
+    its(:client_id) { should eq client_id }
+    its(:api_key) { should eq api_key }
   end
 end
