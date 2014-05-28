3cb6b18a6ab5652568c94103c966c0e7ba324d27
Juan C. Müller <jcmuller@gmail.com>
2014-05-28 11:18:49 -0400

    Use the power of let. DRY.

--- spec/digitalocean_spec.rb
+++ spec/digitalocean_spec.rb
@@ -3,28 +3,27 @@ require 'spec_helper'
 describe Digitalocean do
   subject(:digitalocean) { described_class }
 
+  before do
+    digitalocean.client_id = client_id
+    digitalocean.api_key   = api_key
+  end
+
   describe "defaults" do
-    before do
-      digitalocean.client_id = nil
-      digitalocean.api_key   = nil
-    end
+    let(:client_id) { nil }
+    let(:api_key)   { nil}
 
     its(:api_endpoint) { should eq "https://api.digitalocean.com" }
-    its(:client_id) { should eq "client_id_required" }
-    its(:api_key) { should eq "api_key_required" }
+    its(:client_id)    { should eq "client_id_required" }
+    its(:api_key)      { should eq "api_key_required" }
+
     it { digitalocean::VERSION.should eq "1.0.6" }
   end
 
   describe "setting values" do
-    let(:client_id)   { "1234" }
-    let(:api_key)     { "adf3434938492fjkdfj" }
-
-    before do
-      digitalocean.client_id = client_id
-      digitalocean.api_key   = api_key
-    end
+    let(:client_id) { "1234" }
+    let(:api_key)   { "adf3434938492fjkdfj" }
 
     its(:client_id) { should eq client_id }
-    its(:api_key) { should eq api_key }
+    its(:api_key)   { should eq api_key }
   end
 end
