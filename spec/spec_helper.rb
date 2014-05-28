2d5129d5391b82a31761c4d4c281439fb7946992
Juan C. Müller <jcmuller@gmail.com>
2014-05-28 11:11:24 -0400

    No need to have an empty block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,3 @@ require 'digitalocean'
 require 'securerandom'
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
-
-RSpec.configure do |config|
-  config.before(:suite) do
-  end
-end
-
